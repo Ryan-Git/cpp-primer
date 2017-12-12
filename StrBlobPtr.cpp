644adfd2072a4d8fee4c974dcc3bfdca7031964d
renhongdi <renhongdi@mobike.com>
2017-12-12 11:19:44 +0800

    ch14

--- StrBlobPtr.cpp
+++ StrBlobPtr.cpp
@@ -27,3 +27,27 @@ StrBlobPtr &StrBlobPtr::incr() {
     ++curr;
     return *this;
 }
+
+StrBlobPtr &StrBlobPtr::operator++() {
+    check(curr, "increment past end of StrBlobPtr");
+    ++curr;
+    return *this;
+}
+
+StrBlobPtr &StrBlobPtr::operator--() {
+    --curr;
+    check(-1, "decrement past begin of StrBlobPtr");
+    return *this;
+}
+
+StrBlobPtr StrBlobPtr::operator++(int) {
+    StrBlobPtr ret = *this;
+    ++*this;
+    return ret;
+}
+
+StrBlobPtr StrBlobPtr::operator--(int) {
+    StrBlobPtr ret = *this;
+    --*this;
+    return ret;
+}
